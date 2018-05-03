4ed7e597cda1d5629e486aca0d58f221230ffd3c
Pixeldenker <bram@pixeldenker.nl>
2018-05-03 18:50:46 +0200

    Add check if room exists and isn't full. Let user join room for a valid code

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ const io = require('socket.io')(server)
 const port = process.env.PORT || 3000
 
 io.on('connection', socket => {
-  console.log('new connection!')
+
+  socket.on('room', room => {
+    socket.join(room)
+  })
+
+  socket.on('checkRoom', code => {
+    let senderId = 'id-' + code
+
+    if (io.sockets.adapter.rooms[code] && io.sockets.adapter.rooms[code].length === 1) {
+      // A user is in the room. The user can join the room
+      socket.join(code)
+    } else if (io.sockets.adapter.rooms[code] && io.sockets.adapter.rooms[code].length === 2) {
+      // Two users are in the room. No new user can join a room
+      socket.emit(senderId, 'This room is full')
+    } else {
+      // Room does not exists
+      socket.emit(senderId, 'This room does not exists')
+    }
+  })
 })
 
 server.listen(port, () => console.log(`Listening on port ${port}`))
