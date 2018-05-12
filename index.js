10941d817b4fb144397725f5b3584024833f6508
Pixeldenker <bram@pixeldenker.nl>
2018-05-12 18:05:05 +0200

    Choosen map by client 1 will now be send to client 2

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000
 io.on('connection', socket => {
 
   socket.on('room', room => {
+    // Start new session (room) 
     socket.join(room)
   })
 
@@ -16,6 +17,7 @@ io.on('connection', socket => {
       // A user is in the room. The user can join the room
       socket.join(code)
       socket.emit(senderId, 200)
+      socket.to(code).emit('room-complete', 200)
     } else if (io.sockets.adapter.rooms[code] && io.sockets.adapter.rooms[code].length === 2) {
       // Two users are in the room. No new user can join a room
       socket.emit(senderId, 'This room is full')
@@ -24,6 +26,11 @@ io.on('connection', socket => {
       socket.emit(senderId, 'This room does not exists')
     }
   })
+
+  socket.on('user-choose-map', data => {
+    socket.to(data.code).emit('choosen-map', data)
+  })
+
 })
 
 server.listen(port, () => console.log(`Listening on port ${port}`))
